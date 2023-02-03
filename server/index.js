ca3b92646b892e2be2dce12e10b7524aad93a473
Amit Singhal <amit.singhal@slashrtc.com>
2023-02-03 12:38:21 +0530

    sending 404 with error object to undefined routes

--- server/index.js
+++ server/index.js
@@ -16,6 +16,18 @@ app.use("/users", users);
 app.use("/employees", employees);
 app.use("/admins", admin);
 
+// This should be the last route else any after it won't work
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
