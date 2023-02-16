84562566470f1cf61310c345daa7426cc6d69874
Amit Singhal <amit.singhal@slashrtc.com>
2023-02-16 12:21:47 +0530

    added multer to handle image upload

--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -1,10 +1,27 @@
 const auth = require("../middleware/auth");
 const mongoose = require("mongoose");
-const { Ticket, validate, validateComment } = require("../models/ticket");
+const {
+  Ticket,
+  validate,
+  validateMessageType,
+  validateImageType,
+} = require("../models/ticket");
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 var moment = require("moment");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.post("/create-ticket", async (req, res) => {
   const response = await validate(req.body);
@@ -54,29 +71,64 @@ router.get("/all", auth, async (req, res) => {
   res.send(tickets);
 });
 
-router.post("/comment", auth, async (req, res) => {
-  const response = await validateComment(req.body);
-  if (response.error) {
-    return res.status(400).send(response.errorMessage);
-  }
+router.post("/comment", upload.single("avatar"), async (req, res) => {
+  const file = req.file;
+  console.log(file);
+  const content = file.filename;
 
-  const { id, content, createdBy, userName } = req.body;
-  let createdAt = getTimestamp();
-  let ticket = await Ticket.findById(id);
-  if (!ticket) {
-    return res.send("Can't find ticket id");
-  }
-  await Ticket.updateOne(
-    { _id: id },
-    {
-      $push: {
-        comments: [{ content, createdBy, userName, createdAt }],
-      },
+  const { type } = req.body;
+
+  if (type === "text") {
+    const response = await validateMessageType(req.body);
+    if (response.error) {
+      return res.status(400).send(response.errorMessage);
     }
-  );
-  ticket = await Ticket.findById(id);
+    const { id, content, createdBy, userName, type } = req.body;
 
-  res.status(200).send("Success!");
+    userName.toLowerCase();
+    createdBy.toLowerCase();
+    let createdAt = getTimestamp();
+    let ticket = await Ticket.findById(id);
+    if (!ticket) {
+      return res.send("Can't find ticket id");
+    }
+    await Ticket.updateOne(
+      { _id: id },
+      {
+        $push: {
+          comments: [{ type, content, createdBy, userName, createdAt }],
+        },
+      }
+    );
+    ticket = await Ticket.findById(id);
+
+    res.status(200).send("Success!");
+  } else if (type === "image") {
+    const response = await validateImageType(req.body);
+    if (response.error) {
+      return res.status(400).send(response.errorMessage);
+    }
+    const { id, createdBy, userName, type } = req.body;
+
+    userName.toLowerCase();
+    createdBy.toLowerCase();
+    let createdAt = getTimestamp();
+    let ticket = await Ticket.findById(id);
+    if (!ticket) {
+      return res.send("Can't find ticket id");
+    }
+    await Ticket.updateOne(
+      { _id: id },
+      {
+        $push: {
+          comments: [{ type, content, createdBy, userName, createdAt }],
+        },
+      }
+    );
+    ticket = await Ticket.findById(id);
+
+    res.status(200).send("Success!");
+  }
 });
 
 const getTimestamp = () => {
