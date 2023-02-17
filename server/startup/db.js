fd3f4a012e157db785c45c271f028319f8793c5f
Amit Singhal <amit.singhal@slashrtc.com>
2023-02-17 11:34:51 +0530

    connected to mongo atlas

--- server/startup/db.js
+++ server/startup/db.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose");
 
 module.exports = function () {
+  const uri =
+    "mongodb+srv://amitsin:Slash123@cluster0.0ivaxjw.mongodb.net/helpdesk?retryWrites=true&w=majority";
+
   mongoose
-    .connect("mongodb://127.0.0.1:27017/helpdesk")
-    .then(() => console.log("Connected!"));
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => console.log(err));
+
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "MongoDB connection error:")
+  );
+
+  mongoose.connection.on("disconnected", () =>
+    console.log("MongoDB disconnected")
+  );
 };
