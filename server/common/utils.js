e73ecccfb6c14910c7cf658d08677cd32fdb6d0f
Amit Singhal <amit.singhal@slashrtc.com>
2023-02-20 15:29:03 +0530

    added getTimestamp function

--- server/common/utils.js
+++ server/common/utils.js
@@ -1,8 +1,13 @@
 const jwt = require("jsonwebtoken");
+var moment = require("moment");
 
 module.exports = generateAuthToken = (data) => {
   const token = jwt.sign(data, process.env.JWT_PRIVATE_KEY, {
     expiresIn: "24h",
   });
   return token;
+};
+
+module.exports = getTimestamp = () => {
+  return (unixTimestamp = Math.ceil(moment().valueOf() / 1000));
 };
