e50b221b85cca92bea9240b014e5b4d1e51788ae
Amit Singhal <amit.singhal@slashrtc.com>
2023-03-01 09:31:31 +0530

    forgor password api

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const { CommonUser, validateEmployee } = require("../models/commonUser");
-const { validateResetPassword } = require("../middleware/validator");
+const {
+  validateForgotPassword,
+  validateResetPassword,
+} = require("../middleware/validator");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
@@ -9,6 +12,11 @@ const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
 const generateAuthToken = require("../common/utils");
 const getTimestamp = require("../common/utils");
+var crypto = require("crypto");
+// const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+const API_KEY =
+  "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";
 
 router.post("/register", async (req, res) => {
   const response = await validateEmployee(req.body);
@@ -184,7 +192,7 @@ router.post("/search/:limit/:pageNumber", async (req, res) => {
 
 router.post("/reset/password/:id", async (req, res) => {
   try {
-    const response = await validateResetPassword(req.body);
+    const response = await validateForgotPassword(req.body);
     if (response.error) {
       return res.status(400).send(response.errorMessage);
     }
@@ -221,6 +229,135 @@ router.get("/employee/profile/:id", async (req, res) => {
     res.send(employee);
   } catch {
     res.send("Unable to fetch details");
+  }
+});
+
+// Handle the form submission and send the password reset email
+router.post("/forgot", (req, res) => {
+  // Generate a random token using crypto library
+  crypto.randomBytes(20, (err, buf) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/forgot");
+    }
+
+    const token = buf.toString("hex");
+
+    // Find the user with the given email address in the database
+    CommonUser.findOne({ email: req.body.email }, (err, user) => {
+      if (!user) {
+        req.flash("error", "No account with that email address exists.");
+        return res.redirect("/forgot");
+      }
+
+      // Save the token and expiration time in the user document
+      user.resetPasswordToken = token;
+      user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+      // Save the updated user document in the database
+      user.save((err) => {
+        if (err) {
+          console.log(err);
+          return res.redirect("/forgot");
+        }
+
+        // Send the password reset email using nodemailer library
+        sgMail.setApiKey(API_KEY);
+
+        const mailOptions = {
+          to: user.email,
+          from: "amit.slashrtc@gmail.com",
+          subject: "Node.js Password Reset",
+          text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+            Please click on the following link, or paste it into your browser to complete the process:\n\n
+            http://localhost:3000/reset/${token}\n\n
+            If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+        };
+
+        sgMail
+          .send(mailOptions)
+          .then((response) => console.log("Email sent...", response))
+          .catch((error) =>
+            console.log("Failed to send email", error.response.body)
+          );
+
+        res.send("Email sent");
+      });
+    });
+  });
+});
+
+router.get("/reset/:token", (req, res) => {
+  CommonUser.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() },
+    },
+    (err, user) => {
+      if (!user) {
+        return res.send({
+          error: "Password reset token is invalid or has expired.",
+        });
+      }
+
+      res.status(200).send("Token verified");
+    }
+  );
+});
+
+// Handle the password reset form submission
+router.post("/reset/:token", async (req, res) => {
+  try {
+    const response = await validateResetPassword(req.body);
+    if (response.error) {
+      return res.status(400).send(response.errorMessage);
+    }
+
+    CommonUser.findOne(
+      {
+        resetPasswordToken: req.params.token,
+        resetPasswordExpires: { $gt: Date.now() },
+      },
+      (err, user) => {
+        if (!user) {
+          return res.send({
+            error: "Password reset token is invalid or has expired.",
+          });
+        }
+
+        // Update the user's password and remove the reset token
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+
+        bcrypt.hash(user.password, saltRounds, async function (err, hash) {
+          if (err) console.log(err);
+          user.password = hash;
+          await user.save();
+        });
+
+        // Send a confirmation email to the user
+        sgMail.setApiKey(API_KEY);
+
+        const mailOptions = {
+          to: user.email,
+          from: "amit.slashrtc@gmail.com",
+          subject: "Node.js Password Changed",
+          text: `Hello ${user.name},\n\n
+      This is a confirmation that the password for your account ${user.email} has been changed.\n`,
+        };
+
+        sgMail
+          .send(mailOptions)
+          .then((response) => console.log("Email sent...", response))
+          .catch((error) =>
+            console.log("Failed to send email", error.response.body)
+          );
+
+        res.send("Email sent");
+      }
+    );
+  } catch {
+    res.status(500).send("Failed to reset password");
   }
 });
 
