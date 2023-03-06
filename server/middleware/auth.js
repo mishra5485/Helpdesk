3f86028dba09e48e9eb05c20fab06be653414e3f
Amit Singhal <amit.singhal@slashrtc.com>
2023-03-06 15:37:05 +0530

    fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const headerToken = req.headers.authorization.split(" ")[1];
-  const token =
-    req.body.token || req.query.token || req.params.token || headerToken;
-  if (!token) return res.status(401).send("Access denied. No token provided");
-
   try {
+    const headerToken = req.headers.authorization.split(" ")[1];
+    const token =
+      req.body.token || req.query.token || req.params.token || headerToken;
+    if (!token) return res.status(401).send("Access denied. No token provided");
+
     const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
     req.user = decoded;
     next();
