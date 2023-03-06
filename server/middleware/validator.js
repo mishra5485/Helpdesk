304b1e2b58505b19eeb36e675d8ee4c0b5579eff
Amit Singhal <amit.singhal@slashrtc.com>
2023-03-06 11:31:12 +0530

    updated ticket schema

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -5,7 +5,10 @@ async function validateTicket(ticket) {
   const schema = Joi.object({
     subject: Joi.string().min(5).max(1024).required(),
     body: Joi.string().min(5).required(),
-    user_id: Joi.string().required(),
+    user: Joi.object({
+      user_id: Joi.string().min(2).max(50).required(),
+      userName: Joi.string().min(2).max(50).required(),
+    }).required(),
     department_name: Joi.string().min(2).max(50).required(),
   });
 
